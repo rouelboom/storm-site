3504c4f17ffa1123cb3e27ef334475d2f4c98fc0
Pavel <pavel@ya.ru>
2021-03-07 22:57:36 +0300

    создал модель события

--- events/models.py
+++ events/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+EVENT_TYPE_LIST = [('Warhammer40k', 'WH40K'),
+                   ('SAGA', 'SAGA'),
+                   ('Mallefaux', 'Mallefaux')]
+
+
+class Event(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='author',
+                               verbose_name='Автор события',
+                               help_text='Автор события')
+    type = models.CharField('Тип события', max_length=50,
+                            choices=EVENT_TYPE_LIST)
+    pub_date = models.DateTimeField('Время создания события', auto_now_add=True)
+    event_date = models.DateTimeField('Время проведения события',)
+    opponent = models.ForeignKey(User, on_delete=models.SET_NULL,
+                                 related_name='opponent',
+                                 verbose_name='Текущий опонент',
+                                 help_text='Текущий опонент',
+                                 blank=True,
+                                 null=True)
+
--- events/views.py
+++ events/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from http import HTTPStatus
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse(status=HTTPStatus.OK, content_type='text',
+                        content='Hello, Space Marine!')
